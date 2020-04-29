0b90fcee375f780ca9d239a546b3d9f7d2f796c4
Paul <kontakt@pawelkot.com.pl>
2020-04-30 01:21:04 +0200

    install helment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,39 @@
+const page = `| pawelkot.com.pl`
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `SEO & Python & JavaScript Developer ${page}`,
+    description: `Od ponad 6 lat zajmuje się SEO i SEM. Piszę również kod w Python oraz JavaScript -> React / Gatsby.`,
+    author: `@prott_`,
+    siteUrl: `https://pawelkot.com.pl`,
+    urlPath: ``,
+
+    title_contact: `Kontakt ${page}`,
+    desc_title: `Najszybciej skontaktujesz się ze mną przez maila lub twittera.`,
+    urlPath: `/kontakt/`,
+
+    title_404: `404 ${page}`,
+    desc_404: `Strony nie znaleziono...`,
+    urlPath: `/404/`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-31571413-1",
+        head: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-robots-txt`,
+      options: {
+        host: 'https://pawelkot.com.pl',
+        sitemap: 'https://pawelkot.com.pl/sitemap.xml',
+        policy: [{ userAgent: '*', allow: '/' }]
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -27,6 +56,5 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-sass`,
   ],
 }
