23af7bf16d87996b31df469628db50f9eed18f34
Paul <kontakt@pawelkot.com.pl>
2020-05-01 23:35:41 +0200

    lock zoom

--- src/components/head.js
+++ src/components/head.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Head = () => {
     return(
         <head>
-            
+            <meta name="viewport" content="width=device-width, user-scalable=no"></meta>
         </head>
     )
 }
