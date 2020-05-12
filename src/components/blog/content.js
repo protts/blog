fcd67b7b8e17f33e5d0efd9dcf862f0a1ca6aa8d
Paul <kontakt@pawelkot.com.pl>
2020-05-13 01:18:42 +0200

    style meta

--- src/components/blog/content.js
+++ src/components/blog/content.js
@@ -8,13 +8,15 @@ const Content = ({data}) => {
         <div className="content post-content">
             <header>
                 <h1 className="title is-1" dangerouslySetInnerHTML={{ __html: data.wordpressPost.title }} />
-                <p className="subtitle is-3 has-text-grey">Subtytuł</p>
+                <p className="subtitle is-3 has-text-grey">dfkjhfjshdfjsdkaf</p>
                 <div className="meta-post">
-                    <span>Paweł Kot</span>
+                    <span>{data.wordpressPost.author.name}</span>
                     <span className="separator"></span>
-                    <span>2020, mar 10</span>
+                    <span>{data.wordpressPost.date}</span>
                     <span className="separator"></span>
-                    <span><a href="#">Paweł</a></span>
+                    {data.wordpressPost.categories.map(data => (
+                        <span><a href={data.slug}>{data.name}</a></span>
+                    ))}
                 </div>
             </header>
             <div dangerouslySetInnerHTML={{ __html: data.wordpressPost.content }} />
